4a8dfa53c06b6114df3151404875dad328b41dbc
Arturo Kullmer <arturofks@gmail.com>
2026-09-22 12:49:21 -0300

    fix: set default composition to 'sqrt' from exp3_composition results

--- benchmarks/experiments.py
+++ benchmarks/experiments.py
@@ -19,7 +19,7 @@ from benchmarks.common import COMPOSITIONS
 DATASETS = ('adult', 'sinasc', 'spanish_census', 'chilean_census', 'ipums_1940')
 RHOS = (0.1, 0.5, 1, 2, 5)
 COMPOSITION_RHO = 1
-COMPOSITION = None  # from exp3_composition
+COMPOSITION = 'sqrt'  # exp3_composition, 2026-09-22
 RHO = None          # from exp3_budget
 
 
